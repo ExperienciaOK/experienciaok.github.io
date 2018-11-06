1528748c8f408aa2f466180ce50badaff4a3df99
ExperienciaOK <43549791+ExperienciaOK@users.noreply.github.com>
2018-11-06 13:30:41 -0300

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -1,144 +1,114 @@
-/*
-CONTENIDOS (buscar con %%)
+/*------------------------------------------- NAVEGACIÓN GENERAL ----------------------------------------------------*/
 
-- MODULO:MENU RESPONSIVE
+/*HTML necesario:
+Clase opcional .itemMenu en los anchors que tengan que ser detectados durante el scroll (tienen que estar linkeados en el menu)
 
-- variables globales
+Estructura básica:
 
-document.ready:
-. scroll:
- - Cambiar menú en scroll
-
-- Parallax de sección de imágenes
- 
-- Abrir menú desplegable de proyectos
-- Menu Responsive
--- Click menu hamburguesa
--- Abrir menú desplegable responsive
-
-- Colorear items de menu desplegable on hover
-- Autoscroll flecha banner
-
-- Abrir galeria de imagenes
-
-- Estilos - adiciones en DOM
-
-
+<nav class="mainNav">
+	<ul class="pgwMenu">
+		<li class="mainNav__item"><a href="url">Link externo</a></li>
+		<li class="mainNav__item itemMenu"><a href="#anchor">Link a anchor</a></li>
+		...
+	</ul>
+</nav>
+...
+<section id="anchor" class="itemMenu"></section>
 
 */
 
+var posicionScroll;
+// var menuColorNegro;
+var listaItemsMenu;
+var anchorPreviamenteActivo;
+var anchorActivo;
 
-//------------- MODULO: MENU RESPONSIVE -------------------------
-
-const anchoResponsive = 1200; //en pixeles
-
-//------------- MODULO: MENU RESPONSIVE -------------------------
-
-
-
-//%% variables globales
-
-	//flag para modificación de tamaño de logo
-var logoMod = false;
-var scrollTimer = 0;
-
-
-$(document).ready(function() {
-	
-//------------- INICIO MODULO: MENU RESPONSIVE -------------------------
-	
-	/* %% Cambiar menú en pantallas chicas */
-	
-   if ($('body').width() < anchoResponsive ){
-		$(".c-RM").addClass('c-RM--mobile');
-		$(".c-RM").removeClass('c-RM--desktop');
-   }
-	$(window).resize(function(){     
-       if ($('body').width() < anchoResponsive ){
-			$(".c-RM").addClass('c-RM--mobile');
-			$(".c-RM").removeClass('c-RM--desktop');
-       }
-	   else{
-			$(".c-RM").removeClass('c-RM--mobile');
-			$(".c-RM").addClass('c-RM--desktop');		
-	   }
+function inicializarMainMenu(){
+	$('.pgwMenu').pgwMenu({
+		dropDownLabel : '<span class="white"></span>',
+		viewMoreEnabled : false,
 	});
-//------------- FIN MODULO: MENU RESPONSIVE -------------------------
+}
 
-	$(document).scroll(function() {	
-	var scrollTop = $(window).scrollTop();
-	
-	//%% Cambiar menú en scroll
-	
-			if (scrollTop > 0 && logoMod==false) {
-				$('.logoMain').stop().animate({'height':'15px'},100);
-				logoMod = true;
-			}
-			if (scrollTop <= 0 && logoMod==true){
-				$('.logoMain').stop().animate({'height':'20px'},100);
-				logoMod=false;
-			}
-	});
+function listarAnchors(){
+	listaItemsMenu = $('.itemMenu'); //obtengo un array de elementos DOM, en la forma de un elemento jQuery
+	console.log(listaItemsMenu.length);
+}
 
-
-//%% Parallax de banner inicio
-		console.log("banner inicio:"+$('.MBInicio').length);
-		if($('.MBInicio').length==1){
-			console.log("holu");
-			$('.MBInicio').paroller();
+function chequearAnchor(){
+	posicionScroll = $(document).scrollTop();
+	for(var i=listaItemsMenu.length-1; i>=0; i--){ //recorrer el array desde el final; y buscar el primer (último) elemento del array para el cual se cumple que la posición del scroll es mayor al offset del elemento
+		if(posicionScroll >= $(listaItemsMenu[i]).offset().top-$(".mainNav").height()){
+			anchorActivo=$(listaItemsMenu[i]).attr('id');
+			break; //si se encontró, salir del loop
 		}
+	}
+	if (anchorActivo!=anchorPreviamenteActivo){ //ejecutar cambio de clase solo al cambiar el anchor, no en todos los scrolls
+		$('.mainNav__item.active').removeClass('active');
+		$('.mainNav a[href^="#'+anchorActivo+'"]').parent().addClass('active');
+	}
+	anchorPreviamenteActivo=anchorActivo;
+}
 
-	
-//%% Colorear items de menu desplegable on hover
-	$('.mainNavSubitem').mouseenter(function(){
-		$(this).css({'background-color':'#444'});
-	});
-	$('.mainNavSubitem').mouseleave(function(){
-		$(this).css({'background-color':'transparent'});
-	});
-	
-	
-	
-//%% Autoscroll flecha banner
+// function ajustarColorMenu(){
+	// if(posicionScroll==0||posicionScroll==undefined){
+			// $('.mainNav').removeClass('mainNav--negro');
+			// $('.mainNav').addClass('mainNav--transparente');
+			// menuColorNegro=false;
 
-	$('.flechaMainBanner, .botonConocerMas').click(function(){
-		$('html, body').animate({scrollTop: $('.mainBanner').next().offset().top-55},1000); //desplazar a pos. top de sección sig. a banner, menos el alto del header
-	});
-	
-// %% Abrir galeria de imagenes
-		
-		$('.verGaleria').click(function(){
-			console.log("clickgaleria");
-			$('body').css({'overflow':'hidden'});
-			$('.galeriaImagenes').animate({'opacity':'1'},500);
-			$('.galeriaImagenes').css({'display':'flex'});
-			$('.wrapGaleria').css({'display':'block'});
-		});
-// %% Cerrar galeria de imagenes
-	
-		$('.cruz').click(function(){
-			console.log("clickcruz");
-			$('body').css({'overflow':'auto'});
-			$('.galeriaImagenes').animate({'opacity':'0'},500,function(){
-				$('.galeriaImagenes, .wrapGaleria').css({'display':'none'})
-				});
-			;
-		});
-		
-		$('.wrapGaleria').click(function(){
-			console.log("clickfuera");
-			$('body').css({'overflow':'auto'});
-			$('.galeriaImagenes').animate({'opacity':'0'},500,function(){
-				$('.galeriaImagenes, .wrapGaleria').css({'display':'none'})
-				});
-		}).children().click(function(e) {
-			return false;
-		});
-		
-	
-//%% Estilos - adiciones en DOM
+	// }
+	// else{
+		// if(menuColorNegro==false){ //solo cambiar a clase negro si el estado inmediatamente anterior era transparente
+			// $('.mainNav').removeClass('mainNav--transparente');
+			// $('.mainNav').addClass('mainNav--negro');
+			// menuColorNegro=true;
+		// }
+	// }
+	// console.log(posicionScroll+", "+menuColorNegro);
+// }
 
-	$('.verMas').append(" >>");
-		
+
+
+
+
+//	RUNTIME >>>>>
+
+
+$(document).ready(function(){
+	
+//-------------------------- SETUP
+
+	//setup menú
+	menuColorNegro=false;
+	inicializarMainMenu();
+	listarAnchors();
+	
+	
+//-------------------------- EVENTOS
+
+//Botones
+	
+	//Click en links del menú: scroll suave a anchors
+		$(document).on('click', 'a[href^="#"]', function (event) {		
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: $($.attr(this, 'href')).offset().top-$(".mainNav").height()
+			}, 750);
+		});
+	
+	//Click en botón de "Conocer más"
+	
+		$('.conocerMas').click(function(){
+			$('html, body').animate({
+				scrollTop: $('#queHacemos').offset().top-$(".mainNav").height()
+			}, 500);
+		});
+	
+//Scroll
+
+	$(document).on( "scroll", function(){
+		chequearAnchor();
+	});
+
 });
 
